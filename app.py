b4300384ba06d4d243c007d6d318fdc9ff2ea3f6
xStFtx <you@example.com>
2023-10-10 13:45:39 +0200

    1.8

--- app.py
+++ app.py
@@ -1,50 +1,63 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 import argparse
+import threading
 from main import LoginCracker
 
 app = Flask(__name__, template_folder="templates", static_folder="public")
 
-# Define the LoginCracker instance globally
-login_cracker = None
+app.secret_key = 'some_secret_key'  # for session management
+
+# Storage for results (you might want to use a database in a real-world scenario)
+RESULTS = {}
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    global login_cracker
-
     if request.method == "POST":
-        # Get form data
-        host = request.form["host"]
-        port = int(request.form["port"])
-        username = request.form["username"]
-        password = request.form["password"]
-        charset = request.form["charset"]
-        threads = int(request.form["threads"])
-        processes = int(request.form["processes"])
-        password_length = int(request.form["password_length"])
-        exit_on_first_match = bool(request.form.get("exit_on_first_match"))
-        attack_mode = request.form["attack_mode"]
-        dictionary_file = request.form["dictionary_file"]
+        form_data = {
+            "host": request.form["host"],
+            "port": int(request.form["port"]),
+            "username": request.form["username"],
+            "password": request.form["password"],
+            "charset": request.form["charset"],
+            "threads": int(request.form["threads"]),
+            "processes": int(request.form["processes"]),
+            "password_length": int(request.form["password_length"]),
+            "exit_on_first_match": bool(request.form.get("exit_on_first_match")),
+            "attack_mode": request.form["attack_mode"],
+        }
 
-        # Initialize the LoginCracker instance
-        args = argparse.Namespace(
-            host=host,
-            port=port,
-            username=username,
-            password=password,
-            charset=charset,
-            threads=threads,
-            processes=processes,
-            password_length=password_length,
-            exit_on_first_match=exit_on_first_match,
-            attack_mode=attack_mode,
-            dictionary_file=dictionary_file,
-        )
-        login_cracker = LoginCracker(args)
+        if form_data["attack_mode"] in ["hybrid", "dict"]:
+            form_data["dictionary_file"] = request.form["dictionary_file"]
+        else:
+            form_data["dictionary_file"] = None
 
-        # Start the cracking process (you may want to use a background task)
-        login_cracker.run()
+        # Run in the background
+        thread = threading.Thread(target=run_cracker, args=(form_data,))
+        thread.start()
+
+        # Store thread in session for retrieval
+        session['task_id'] = thread.ident
+        flash("Cracking process started! Check back later for results.")
+
+        return redirect(url_for('index'))
 
     return render_template("index.html")
+
+def run_cracker(form_data):
+    args = argparse.Namespace(**form_data)
+    login_cracker = LoginCracker(args)
+    result = login_cracker.run()
+    RESULTS[session['task_id']] = result
+
+@app.route("/results/")
+def results():
+    task_id = session.get('task_id')
+    result = RESULTS.get(task_id)
+
+    if not result:
+        return "No results yet. Please check back later."
+
+    return f"Cracked credentials: Username: {result[0]}, Password: {result[1]}"
 
 if __name__ == "__main__":
     app.run(debug=True)
--- main.py
+++ main.py
@@ -1,30 +1,21 @@
 import argparse
-import multiprocessing
-import socks
-import socket
-import threading
 import logging
 import sys
 import string
 import time
-import concurrent.futures
 import itertools
-import secrets
 import bcrypt
 
 # Constants
 DEFAULT_NUM_THREADS = 5
 DEFAULT_PASSWORD_MIN_LENGTH = 1
 DEFAULT_PASSWORD_MAX_LENGTH = 5
-DEFAULT_NUM_PROCESSES = 4
 DEFAULT_LOGGING_LEVEL = logging.INFO
-PROXY_TIMEOUT = 10
 
 
 class LoginCracker:
     def __init__(self, args):
         self.args = args
-        self.exit_event = multiprocessing.Event()
         self.custom_charset = args.charset
         self.init_logging()
 
@@ -36,24 +27,6 @@ class LoginCracker:
             stream=sys.stdout
         )
 
-    def proxy_connect(self, target_host, target_port):
-        """Connect using SOCKS proxy."""
-        with socks.socksocket(socket.AF_INET, socket.SOCK_STREAM) as proxy_client:
-            proxy_client.set_proxy(socks.SOCKS5, "localhost", 1080)
-            proxy_client.connect((target_host, target_port))
-            proxy_client.send(f"GET / HTTP/1.1\r\nHost: {target_host}\r\n\r\n".encode())
-            return proxy_client.recv(4096).decode("utf-8")
-
-    def socks_proxy_thread(self):
-        """Run a SOCKS proxy thread."""
-        try:
-            response = self.proxy_connect(self.args.host, self.args.port)
-            logging.info(f"Received from target: {response}")
-        except Exception as e:
-            logging.error(f"Proxy thread error: {e}")
-        finally:
-            self.exit_event.set()
-
     def is_valid_login(self, username, password):
         """Check if the given login is valid."""
         stored_password_hash = bcrypt.hashpw(self.args.password.encode(), bcrypt.gensalt())
@@ -61,20 +34,19 @@ class LoginCracker:
 
     def dictionary_attack(self):
         """Attack using a dictionary."""
-        with open(self.args.dictionary_file, 'r') as file:
-            for password in file:
-                password = password.strip()
-                if self.is_valid_login(self.args.username, password):
-                    return self.args.username, password
-                if self.exit_event.is_set():
-                    break
+        try:
+            with open(self.args.dictionary_file, 'r') as file:
+                for password in file:
+                    password = password.strip()
+                    if self.is_valid_login(self.args.username, password):
+                        return self.args.username, password
+        except Exception as e:
+            logging.error(f"Dictionary attack error: {e}")
         return None
 
     def brute_force_attack(self, length):
         """Brute force attack using a specified length."""
         for candidate in itertools.product(self.custom_charset, repeat=length):
-            if self.exit_event.is_set():
-                break
             candidate_password = ''.join(candidate)
             if self.is_valid_login(self.args.username, candidate_password):
                 return self.args.username, candidate_password
@@ -88,58 +60,35 @@ class LoginCracker:
                 return result
         return None
 
-    def hybrid_attack(self):
-        """Run a hybrid attack of dictionary and brute force."""
-        result = self.dictionary_attack()
-        if not result:
-            result = self.run_brute_force()
-        return result
-
     def run(self):
         """Main execution method."""
-        threads = [threading.Thread(target=self.socks_proxy_thread) for _ in range(self.args.threads)]
-        for thread in threads:
-            thread.start()
+        start_time = time.time()
 
-        try:
-            for thread in threads:
-                thread.join()
+        attack_map = {
+            'dictionary': self.dictionary_attack,
+            'hybrid': lambda: self.dictionary_attack() or self.run_brute_force(),
+            'bruteforce': self.run_brute_force
+        }
 
-            start_time = time.time()
+        result = attack_map.get(self.args.attack_mode, lambda: None)()
 
-            attack_map = {
-                'dictionary': self.dictionary_attack,
-                'hybrid': self.hybrid_attack,
-                'bruteforce': self.run_brute_force
-            }
+        end_time = time.time()
 
-            result = attack_map.get(self.args.attack_mode, lambda: None)()
-
-            end_time = time.time()
-
-            # Output results
-            if result:
-                print(f"\nLogin cracked - Username: {result[0]}, Password: {result[1]}")
-            else:
-                print("Login not found.")
-            print(f"Execution time: {end_time - start_time:.2f} seconds")
-
-        except KeyboardInterrupt:
-            logging.info("Received KeyboardInterrupt. Exiting gracefully...")
-            self.exit_event.set()
-            sys.exit(0)
+        # Output results
+        if result:
+            print(f"\nLogin cracked - Username: {result[0]}, Password: {result[1]}")
+        else:
+            print("Login not found.")
+        print(f"Execution time: {end_time - start_time:.2f} seconds")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Parallel Login Cracker")
 
-    parser.add_argument("--host", default="example.com", help="Target host")
-    parser.add_argument("--port", type=int, default=80, help="Target port")
     parser.add_argument("--username", default="admin", help="Target username")
     parser.add_argument("--password", default="12345", help="Target password to crack")
     parser.add_argument("--charset", default=string.ascii_letters + string.digits + string.punctuation,
                         help="Character set for brute force")
-    parser.add_argument("--threads", type=int, default=DEFAULT_NUM_THREADS, help="Number of threads")
     parser.add_argument("--password-min-length", type=int, default=DEFAULT_PASSWORD_MIN_LENGTH, help="Minimum password length for brute force")
     parser.add_argument("--password-max-length", type=int, default=DEFAULT_PASSWORD_MAX_LENGTH, help="Maximum password length for brute force")
     parser.add_argument("--log-level", default=DEFAULT_LOGGING_LEVEL, help="Logging level")
